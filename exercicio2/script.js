fd0357de8e1fe2c988d9472ebfbfa6ca7167e738
slvgs <gabriellasilveriosantana15@gmail.com>
2022-09-13 15:44:11 -0300

    Concluidos

---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+const number = Number(prompt("Insirra um número!"));
+const tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i in tabuada){
+    i++
+     console.log(`${number} X ${i} = ${number * i}`)
+
+ }
+
+
+// for(let i = 0; i < 10; i++){
+//     console.log(`${number} X ${i +1} = ${number * i+1}`)
+// }
